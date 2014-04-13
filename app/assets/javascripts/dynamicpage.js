e7113f60d6b7216f07422513a189d656132a237f
Jbleez <liljl0902@gmail.com>
2014-04-13 10:35:11 -0400

    website pushed

---
+++ app/assets/javascripts/dynamicpage.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    if(Modernizr.history){
+
+    var newHash      = "",
+        $mainContent = $(".row text-center"),
+        $pageWrap    = $("#landing"),
+        baseHeight   = 0,
+        $el;
+        
+    $pageWrap.height($pageWrap.height());
+    baseHeight = $pageWrap.height() - $mainContent.height();
+    
+    $("form").delegate("button", "click", function() {
+        _link = $(this).attr("id");
+        history.pushState(null, null, _link);
+        loadContent(_link);
+        return false;
+    });
+
+    function loadContent(href){
+        $mainContent
+                .find("#guts")
+                .fadeOut(200, function() {
+                    $mainContent.hide().load(href + " #guts", function() {
+                        $mainContent.fadeIn(200, function() {
+                            $pageWrap.animate({
+                                height: baseHeight + $mainContent.height() + "px"
+                            });
+                        });
+                        
+                        console.log(href);
+                    
+                    });
+                });
+    }
+    
+    $(window).bind('popstate', function(){
+       _link = location.pathname.replace(/^.*[\\\/]/, ''); //get filename only
+       loadContent(_link);
+    });
+
+} // otherwise, history is not supported, so nothing fancy here.
+
+    
+});
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,5 @@
+/*$(function() {  
+    $(".search").click(function() {  
+         $('#guts').fadeOut(500, function(){$('#guts').fadeIn(500);});
+    });  
+}); */
